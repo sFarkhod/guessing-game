4029318f50134584dae147a8c34d6f49128e86d9
sFarkhod <93736788+sFarkhod@users.noreply.github.com>
2022-08-11 15:03:26 +0500

    just a commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.gnum = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.gnum;
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.gnum;
+        this.guess();
     }
 }
 
